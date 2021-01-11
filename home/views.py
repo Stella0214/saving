178cd400436f84c5ec839fbc68646729a816f49b
power515 <power515@163.com>
2021-01-12 07:15:42 +0800

    marc 20210112-2 add login

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.conf import settings
 from django.shortcuts import redirect
+from . import models
 
 # Create your views here.
 
@@ -17,7 +18,28 @@ class HomeView(View):
         return render(request, 'home/main.html', context)
 
 def login(request):
-    pass
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        message = '请检查填写的内容！'
+        if username.strip() and password:
+            # 用户名字符合法性验证
+            # 密码长度验证
+            # 更多的其它验证.....
+            try:
+                user = models.User.objects.get(name=username) #???
+            except :
+                message = '用户不存在！'
+                return render(request, 'home/login.html', {'message': message})
+
+            if user.password == password:
+                print(username, password)
+                return redirect('/')
+            else:
+                message = '密码不正确！'
+                return render(request, 'home/login.html', {'message': message})
+        else:
+            return render(request, 'home/login.html', {'message': message})
     return render(request, 'home/login.html')
 
 def register(request):
