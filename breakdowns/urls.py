9afee363719a74e3c4c05e294d0a700d2392ba18
power515 <power515@163.com>
2021-01-13 23:03:29 +0800

    marc 20210113 adminlet

--- breakdowns/urls.py
+++ breakdowns/urls.py
@@ -6,7 +6,7 @@ app_name = 'breakdowns'
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('index/', views.index, name='index'),
-    path('detail/<int:asset_id>/', views.detail, name="detail"),
+    path('detail/<int:costbreakdown_id>/', views.detail, name="detail"),
     path('', views.dashboard),   
 ]
 
